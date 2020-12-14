b8251d682ad34e2909c076eedf12e4a39badf4e3
Tom Netzband <tomnez@gmail.com>
2020-12-14 15:11:11 -0600

    Add test for inject decorator fallback

--- addon/helpers/consume-context.js
+++ addon/helpers/consume-context.js
@@ -2,7 +2,8 @@ import Helper from '@ember/component/helper';
 import { getProviderFor } from '../-private/get-provider-for';
 
 export default class ContextConsumerHelper extends Helper {
-  compute([key], { fallback }) {
+  compute([key], opts) {
+    const fallback = opts ? opts.fallback : null;
     const provider = getProviderFor(this, key, fallback);
 
     return provider ? provider.value : fallback;
--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,10 @@
 import { getProviderFor } from './-private/get-provider-for';
 
-export function inject(key, { fallback }) {
+export function inject(key, opts) {
   return function decorator() {
     return {
       get() {
+        const fallback = opts ? opts.fallback : null;
         const provider = getProviderFor(this, key, fallback);
 
         return provider ? provider.value : fallback;
